0cb1cf455a5a14b498c59a9dcac465a64b7ed071
Underscoore <ajaykumar.bit.1995@gmail.com>
2024-08-04 12:09:21 -0700

    write 3 solutions for array and hasing problem 242 leetcode

--- array_and_hashing/src/main.rs
+++ array_and_hashing/src/main.rs
@@ -1,24 +1,13 @@
+mod solutions;
+use solutions::solution1;
+use solutions::solution2;
+use solutions::solution3;
+
 fn main() {
     let s1: String = "ajay".to_string();
     let s2: String = "yaja".to_string();
 
-    println!("{}",is_anagram(s1, s2));
-}
-
-fn is_anagram(string1: String, string2: String) -> bool{
-    let mut char_vec1:Vec<char> = string1.chars().collect();
-    let mut char_vec2:Vec<char> = string2.chars().collect();
-
-    if string1.len() != string2.len() {
-        return false;
-    }
-
-    if char_vec1.len() > 0 && char_vec2.len() > 0{
-        char_vec1.sort();
-        char_vec2.sort();
-        return char_vec1 == char_vec2;
-    } else {
-        return false;
-    }
-
+    println!("{}", solution1::is_anagram(&s1, &s2));
+    println!("{}", solution2::is_anagram(&s1, &s2));
+    println!("{}", solution3::is_anagram(&s1, &s2));
 }
---
+++ array_and_hashing/src/solutions/solution3.rs
@@ -0,0 +1,59 @@
+// Here in this sliution I am going to use a single HashMap
+// Fist I will enter vales in HashMap: Ex if a letter `a` occuress 2 times then key `a`: 2
+// then I will be removing the values from HashMap to the corresponding key
+
+use std::collections::HashMap;
+
+pub fn is_anagram(string1: &String, string2: &String) -> bool{
+    let mut map = HashMap::new();
+    if string1.len() != string2.len(){
+        return false;
+    }
+
+    for char in string1.chars(){
+        *map.entry(char).or_insert(0) +=1;
+    }
+
+    for char in string2.chars(){
+        *map.entry(char).or_insert(0) -=1;
+    }
+    for (_key, value) in map{
+        if value != 0{
+            return false;
+        }
+    }
+    true    
+}
+
+#[cfg(test)]
+mod tests{
+    use super::is_anagram;
+
+
+    #[test]
+    fn unit_is_anagram_pos_s3(){
+        let s1: String = "lowlevel".to_string();
+        let s2: String = "levellow".to_string();
+        assert_eq!(is_anagram(&s1, &s2), true);
+    }
+    #[test]
+    fn unit_is_anagram_neg_s3(){
+        let s1: String = "anagram".to_string();
+        let s2: String = "margano".to_string();
+        assert_eq!(is_anagram(&s1, &s2), false);
+    }
+
+    #[test]
+    fn unit_is_anagram_ueq_s3(){
+        let s1: String = "anagr".to_string();
+        let s2: String = "margana".to_string();
+        assert_eq!(is_anagram(&s1, &s2), false);
+    }
+    #[test]
+    fn unit_is_anagram_emp_s3(){
+        let s1: String = "".to_string();
+        let s2: String = "".to_string();
+        assert_eq!(is_anagram(&s1, &s2), true);
+    }
+
+}
