dd087e55059c16d7153900a60f32e170e0a311c1
Underscoore <ajaykumar.bit.1995@gmail.com>
2024-08-04 10:35:52 -0700

    array and hasging method 1: done

--- array_and_hashing/src/main.rs
+++ array_and_hashing/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let s1: String = "ajay".to_string();
+    let s2: String = "yaja".to_string();
+
+    println!("{}",is_anagram(s1, s2));
+}
+
+fn is_anagram(string1: String, string2: String) -> bool{
+    let mut char_vec1:Vec<char> = string1.chars().collect();
+    let mut char_vec2:Vec<char> = string2.chars().collect();
+
+    if string1.len() != string2.len() {
+        return false;
+    }
+
+    if char_vec1.len() > 0 && char_vec2.len() > 0{
+        char_vec1.sort();
+        char_vec2.sort();
+        return char_vec1 == char_vec2;
+    } else {
+        return false;
+    }
+
 }
