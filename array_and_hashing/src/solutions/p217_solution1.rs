900574768f8f30bc4df308de05205602c4986db1
Underscoore <ajaykumar.bit.1995@gmail.com>
2024-08-06 11:40:45 -0700

    217: Is duplicate integer added 2 solutions

---
+++ array_and_hashing/src/solutions/p217_solution1.rs
@@ -0,0 +1,63 @@
+// The PROBELM
+// Duplicate Integer
+/*
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+*/
+
+use std::collections::HashMap; 
+
+// Purposed solution:
+// Forst I all insert all the element in HashMap 
+// Then check if a value against each key is > 1
+// If value is greater then 1 then the value is appearing more then once;
+
+pub fn is_duplicate_integer(numbers: &Vec<u32>) -> bool{
+    let mut map = HashMap::new();
+    if numbers.len() > 0{
+        for num in numbers{
+            *map.entry(num).or_insert(0) +=1; 
+        }  
+        for (_key, value) in map{
+            if value > 1 {
+                return false;
+            }
+        }
+        return true;
+    }
+    false
+}
+
+
+#[cfg(test)]
+mod tests{
+    use crate::solutions::p217_solution1::is_duplicate_integer;
+
+    #[test]
+    fn unit_is_duplicate_integer_pos_s1(){
+        let numbers: Vec<u32> = vec![1, 2, 3];
+        assert_eq!(is_duplicate_integer(&numbers), true);
+
+    }
+    #[test]
+    fn unit_is_duplicate_integer_neg_s1(){
+        let numbers: Vec<u32> = vec![1, 2, 3, 3];
+        assert_eq!(is_duplicate_integer(&numbers), false);
+    }
+    #[test]
+    fn unit_is_duplicate_integer_emp_s1(){
+        let numbers: Vec<u32> = vec![];
+        assert_eq!(is_duplicate_integer(&numbers), false);
+    }
+
+}
--- array_and_hashing/src/solutions/p242_solution3.rs
+++ array_and_hashing/src/solutions/p242_solution3.rs
@@ -4,56 +4,54 @@
 
 use std::collections::HashMap;
 
-pub fn is_anagram(string1: &String, string2: &String) -> bool{
+pub fn is_anagram(string1: &String, string2: &String) -> bool {
     let mut map = HashMap::new();
-    if string1.len() != string2.len(){
+    if string1.len() != string2.len() {
         return false;
     }
 
-    for char in string1.chars(){
-        *map.entry(char).or_insert(0) +=1;
+    for char in string1.chars() {
+        *map.entry(char).or_insert(0) += 1;
     }
 
-    for char in string2.chars(){
-        *map.entry(char).or_insert(0) -=1;
+    for char in string2.chars() {
+        *map.entry(char).or_insert(0) -= 1;
     }
-    for (_key, value) in map{
-        if value != 0{
+    for (_key, value) in map {
+        if value != 0 {
             return false;
         }
     }
-    true    
+    true
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::is_anagram;
 
-
     #[test]
-    fn unit_is_anagram_pos_s3(){
+    fn unit_is_anagram_pos_s3() {
         let s1: String = "lowlevel".to_string();
         let s2: String = "levellow".to_string();
         assert_eq!(is_anagram(&s1, &s2), true);
     }
     #[test]
-    fn unit_is_anagram_neg_s3(){
+    fn unit_is_anagram_neg_s3() {
         let s1: String = "anagram".to_string();
         let s2: String = "margano".to_string();
         assert_eq!(is_anagram(&s1, &s2), false);
     }
 
     #[test]
-    fn unit_is_anagram_ueq_s3(){
+    fn unit_is_anagram_ueq_s3() {
         let s1: String = "anagr".to_string();
         let s2: String = "margana".to_string();
         assert_eq!(is_anagram(&s1, &s2), false);
     }
     #[test]
-    fn unit_is_anagram_emp_s3(){
+    fn unit_is_anagram_emp_s3() {
         let s1: String = "".to_string();
         let s2: String = "".to_string();
         assert_eq!(is_anagram(&s1, &s2), true);
     }
-
 }
